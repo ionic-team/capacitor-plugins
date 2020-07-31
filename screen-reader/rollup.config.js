925ea6b8d8a87a6f08a0a855bd6e8987fca357f7
Daniel Imhoff <dwieeb@gmail.com>
2020-07-31 15:02:48 -0700

    change iife export

--- screen-reader/rollup.config.js
+++ screen-reader/rollup.config.js
@@ -5,7 +5,7 @@ export default {
   output: {
     file: 'dist/plugin.js',
     format: 'iife',
-    name: 'capacitorPlugin', // TODO: change this
+    name: 'capacitorScreenReader',
     globals: {
       '@capacitor/core': 'capacitorExports',
     },
