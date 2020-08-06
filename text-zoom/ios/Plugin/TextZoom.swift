22926d6ff684de1e4a060b2dbae649039fbebb8b
Daniel Imhoff <dwieeb@gmail.com>
2020-08-06 12:07:09 -0700

    remove unused ios native code and web implementation

--- text-zoom/ios/Plugin/TextZoom.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-@objc public class TextZoom: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
-    }
-}
--- text-zoom/src/index.ts
+++ text-zoom/src/index.ts
@@ -5,13 +5,11 @@ import {
 } from '@capacitor/core';
 
 import { TextZoomPlugin } from './definitions';
-import { TextZoomWeb } from './web';
 import { TextZoomIOS } from './ios';
 
 const implementations: PluginImplementations<TextZoomPlugin> = {
   android: Plugins.TextZoom,
   ios: new TextZoomIOS(),
-  web: new TextZoomWeb(),
 };
 
 const TextZoom = registerPlugin(
