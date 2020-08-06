3619593afa9831b7839e80e399e354a952ff9305
Daniel Imhoff <dwieeb@gmail.com>
2020-08-06 13:24:33 -0700

    jsdoc

--- text-zoom/src/definitions.ts
+++ text-zoom/src/definitions.ts
@@ -5,7 +5,30 @@ declare module '@capacitor/core' {
 }
 
 export interface TextZoomPlugin {
+  /**
+   * Get the current zoom level.
+   *
+   * Zoom levels are represented as a decimal (e.g. 1.2 is 120%).
+   *
+   * @since 0.0.1
+   */
   get(): Promise<{ value: number }>;
+
+  /**
+   * Get the preferred zoom level.
+   *
+   * Zoom levels are represented as a decimal (e.g. 1.2 is 120%).
+   *
+   * @since 0.0.1
+   */
   getPreferred(): Promise<{ value: number }>;
+
+  /**
+   * Set the current zoom level.
+   *
+   * Zoom levels are represented as a decimal (e.g. 1.2 is 120%).
+   *
+   * @since 0.0.1
+   */
   set(options: { value: number }): Promise<void>;
 }
