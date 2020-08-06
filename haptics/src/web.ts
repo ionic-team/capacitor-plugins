cfad79b64d4503cce3a22f9ab3571c4517f5aa23
jcesarmobile <jcesarmobile@gmail.com>
2020-08-06 17:13:51 +0200

    don't resolve

--- haptics/src/web.ts
+++ haptics/src/web.ts
@@ -13,19 +13,16 @@ export class HapticsWeb extends WebPlugin implements HapticsPlugin {
 
   async impact(options: HapticsImpactOptions): Promise<void> {
     console.log('impact options', options);
-    return Promise.resolve();
   }
 
   async notification(options: HapticsNotificationOptions): Promise<void> {
     console.log('notification options', options);
-    return Promise.resolve();
   }
 
   async vibrate(options?: VibrateOptions): Promise<void> {
     if (navigator.vibrate) {
       const duration = options?.duration || 300;
       navigator.vibrate(duration);
-      return Promise.resolve();
     } else {
       return Promise.reject('Vibrate API not available in this browser');
     }
@@ -33,16 +30,13 @@ export class HapticsWeb extends WebPlugin implements HapticsPlugin {
 
   async selectionStart(): Promise<void> {
     console.log('selectionStart');
-    return Promise.resolve();
   }
 
   async selectionChanged(): Promise<void> {
     console.log('selectionChanged');
-    return Promise.resolve();
   }
 
   async selectionEnd(): Promise<void> {
     console.log('selectionEnd');
-    return Promise.resolve();
   }
 }
