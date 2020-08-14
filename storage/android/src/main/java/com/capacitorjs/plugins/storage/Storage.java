443b4a24848b9284436adc583a98cad6bf74afce
Daniel Imhoff <dwieeb@gmail.com>
2020-08-14 11:20:00 -0700

    android implementation

--- storage/android/src/main/java/com/capacitorjs/plugins/storage/Storage.java
+++ storage/android/src/main/java/com/capacitorjs/plugins/storage/Storage.java
@@ -1,8 +1,45 @@
 package com.capacitorjs.plugins.storage;
 
-public class Storage {
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import java.util.Set;
 
-    public String echo(String value) {
-        return value;
+public class Storage {
+    private static final String PREFERENCES_NAME = "CapacitorStorage";
+    private SharedPreferences preferences;
+
+    interface StorageOperation {
+        void execute(SharedPreferences.Editor editor);
+    }
+
+    Storage(Context context) {
+        preferences = context.getSharedPreferences(PREFERENCES_NAME, Activity.MODE_PRIVATE);
+    }
+
+    public String get(String key) {
+        return preferences.getString(key, null);
+    }
+
+    public void set(String key, String value) {
+        executeOperation(editor -> editor.putString(key, value));
+    }
+
+    public void remove(String key) {
+        executeOperation(editor -> editor.remove(key));
+    }
+
+    public Set<String> keys() {
+        return preferences.getAll().keySet();
+    }
+
+    public void clear() {
+        executeOperation(SharedPreferences.Editor::clear);
+    }
+
+    private void executeOperation(StorageOperation op) {
+        SharedPreferences.Editor editor = preferences.edit();
+        op.execute(editor);
+        editor.apply();
     }
 }
--- storage/android/src/main/java/com/capacitorjs/plugins/storage/StoragePlugin.java
+++ storage/android/src/main/java/com/capacitorjs/plugins/storage/StoragePlugin.java
@@ -1,21 +1,83 @@
 package com.capacitorjs.plugins.storage;
 
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import java.util.Set;
+import org.json.JSONException;
 
 @NativePlugin(name = "Storage")
 public class StoragePlugin extends Plugin {
-    private Storage implementation = new Storage();
+    private Storage storage;
+
+    @Override
+    public void load() {
+        storage = new Storage(getContext());
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void get(PluginCall call) {
+        String key = call.getString("key");
+        if (key == null) {
+            call.reject("Must provide key");
+            return;
+        }
+
+        String value = storage.get(key);
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("value", value == null ? JSObject.NULL : value);
         call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void set(PluginCall call) {
+        String key = call.getString("key");
+        if (key == null) {
+            call.reject("Must provide key");
+            return;
+        }
+
+        String value = call.getString("value");
+        storage.set(key, value);
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void remove(PluginCall call) {
+        String key = call.getString("key");
+        if (key == null) {
+            call.reject("Must provide key");
+            return;
+        }
+
+        storage.remove(key);
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void keys(PluginCall call) {
+        Set<String> keySet = storage.keys();
+        String[] keys = keySet.toArray(new String[0]);
+
+        JSObject ret = new JSObject();
+        try {
+            ret.put("keys", new JSArray(keys));
+        } catch (JSONException ex) {
+            call.reject("Unable to create key array.");
+            return;
+        }
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void clear(PluginCall call) {
+        storage.clear();
+        call.resolve();
     }
 }
