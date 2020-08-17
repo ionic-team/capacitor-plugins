0a4e4229f10a9787c8ab00ac4340decb71f98439
Ian Keith <iankeith@gmail.com>
2020-08-17 17:38:18 -0400

    Updating typescript documentation.

--- network/src/definitions.ts
+++ network/src/definitions.ts
@@ -2,12 +2,16 @@ import { PluginListenerHandle } from '@capacitor/core';
 
 export interface NetworkPlugin {
   /**
-   * Query the current network status
+   * Query the current status of the network connection.
+   *
+   * @since 1.0.0
    */
   getStatus(): Promise<NetworkStatus>;
 
   /**
-   * Listen for network status change events
+   * Listen for changes in the network connection.
+   *
+   * @since 1.0.0
    */
   addListener(
     eventName: 'networkStatusChange',
@@ -15,14 +19,26 @@ export interface NetworkPlugin {
   ): PluginListenerHandle;
 
   /**
-   * Remove all native listeners for this plugin
+   * Remove all listeners (including the network status changes) for this plugin.
+   *
+   * @since 1.0.0
    */
   removeAllListeners(): void;
 }
 
+/**
+ * Represents the state and type of the network connection.
+ *
+ * @since 1.0.0
+ */
 export interface NetworkStatus {
   connected: boolean;
   connectionType: 'wifi' | 'cellular' | 'none' | 'unknown';
 }
 
+/**
+ * Callback to receive the status change notifications.
+ *
+ * @since 1.0.0
+ */
 export type NetworkStatusChangeCallback = (status: NetworkStatus) => void;
