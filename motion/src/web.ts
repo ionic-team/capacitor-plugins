8d42990ac6664cb8a4b6373146c307747c2ceafa
jcesarmobile <jcesarmobile@gmail.com>
2020-08-19 17:56:14 +0200

    Add web implementation

--- motion/src/web.ts
+++ motion/src/web.ts
@@ -4,10 +4,7 @@ import { MotionPlugin } from './definitions';
 export class MotionWeb extends WebPlugin implements MotionPlugin {
   constructor() {
     super({ name: 'Motion' });
-  }
-
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+    this.registerWindowListener('devicemotion', 'accel');
+    this.registerWindowListener('deviceorientation', 'orientation');
   }
 }
