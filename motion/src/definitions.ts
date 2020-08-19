58ee5bbe422478a1baec436dd1a6fbe492fe8773
jcesarmobile <jcesarmobile@gmail.com>
2020-08-19 17:55:50 +0200

    Add definitions

--- motion/src/definitions.ts
+++ motion/src/definitions.ts
@@ -1,3 +1,5 @@
+import { PluginListenerHandle } from '@capacitor/core';
+
 declare module '@capacitor/core' {
   interface PluginRegistry {
     Motion: MotionPlugin;
@@ -5,5 +7,53 @@ declare module '@capacitor/core' {
 }
 
 export interface MotionPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  /**
+   * Listen for accelerometer data
+   */
+  addListener(
+    eventName: 'accel',
+    listenerFunc: (event: MotionEventResult) => void,
+  ): PluginListenerHandle;
+  /**
+   * Listen for device orientation change (compass heading, etc.)
+   */
+  addListener(
+    eventName: 'orientation',
+    listenerFunc: (event: MotionOrientationEventResult) => void,
+  ): PluginListenerHandle;
+
+  /**
+   * Remove all native listeners for this plugin
+   */
+  removeAllListeners(): void;
+}
+
+export type MotionWatchOrientationCallback = (
+  accel: MotionOrientationEventResult,
+) => void;
+export type MotionWatchAccelCallback = (accel: MotionEventResult) => void;
+
+export interface MotionOrientationEventResult {
+  alpha: number;
+  beta: number;
+  gamma: number;
+}
+
+export interface MotionEventResult {
+  acceleration: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  accelerationIncludingGravity: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  rotationRate: {
+    alpha: number;
+    beta: number;
+    gamma: number;
+  };
+  interval: number;
 }
