a80a79c303a953ca75adf822952cf9314350059b
Ian Keith <iankeith@gmail.com>
2020-08-19 16:15:11 -0400

    Raw plugin implementation import from capacitor.

--- browser/ios/Plugin/BrowserPlugin.m
+++ browser/ios/Plugin/BrowserPlugin.m
@@ -1,8 +1,9 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(BrowserPlugin, "Browser",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+CAP_PLUGIN(CAPBrowserPlugin, "Browser",
+  CAP_PLUGIN_METHOD(open, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(close, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(prefetch, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnNone);
 )
