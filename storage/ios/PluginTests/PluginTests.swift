5a6498eb80d7fecaf52a3f16c96d05ceb48f0548
Dan Imhoff <dwieeb@gmail.com>
2020-08-19 11:03:57 -0700

    Update storage/ios/PluginTests/PluginTests.swift
    Co-authored-by: jcesarmobile <jcesarmobile@gmail.com>

--- storage/ios/PluginTests/PluginTests.swift
+++ storage/ios/PluginTests/PluginTests.swift
@@ -13,14 +13,4 @@ class StorageTests: XCTestCase {
         super.tearDown()
     }
 
-    func testEcho() {
-        // This is an example of a functional test case for a plugin.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-
-        let implementation = Storage()
-        let value = "Hello, World!"
-        let result = implementation.echo(value)
-
-        XCTAssertEqual(value, result)
-    }
 }
