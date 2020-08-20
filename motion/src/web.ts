21ace1670b803eec3a8d41d06c32933a170bf4be
jcesarmobile <jcesarmobile@gmail.com>
2020-08-20 21:43:19 +0200

    feat: Motion plugin (#17)

---
+++ motion/src/web.ts
@@ -0,0 +1,10 @@
+import { WebPlugin } from '@capacitor/core';
+import { MotionPlugin } from './definitions';
+
+export class MotionWeb extends WebPlugin implements MotionPlugin {
+  constructor() {
+    super({ name: 'Motion' });
+    this.registerWindowListener('devicemotion', 'accel');
+    this.registerWindowListener('deviceorientation', 'orientation');
+  }
+}
