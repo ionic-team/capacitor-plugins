9dfdd85b39c0df90db416a4bed094d03640fbe34
Dan Imhoff <dwieeb@gmail.com>
2020-08-21 15:08:12 -0700

    feat: Storage plugin (#15)

---
+++ storage/src/index.ts
@@ -0,0 +1,18 @@
+import {
+  Plugins,
+  PluginImplementations,
+  registerPlugin,
+} from '@capacitor/core';
+
+import { StoragePlugin } from './definitions';
+import { StorageWeb } from './web';
+
+const implementations: PluginImplementations<StoragePlugin> = {
+  android: Plugins.Storage,
+  ios: Plugins.Storage,
+  web: new StorageWeb(),
+};
+
+const Storage = registerPlugin('Storage', implementations).getImplementation();
+
+export { Storage };
