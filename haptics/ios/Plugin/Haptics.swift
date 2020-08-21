d3a1a67d98c6afaa26e9ebb7983951d24e2dec01
jcesarmobile <jcesarmobile@gmail.com>
2020-08-21 19:23:42 +0200

    Apply Ian suggestions for iOS

--- haptics/ios/Plugin/Haptics.swift
+++ haptics/ios/Plugin/Haptics.swift
@@ -5,67 +5,33 @@ import UIKit
 
     var selectionFeedbackGenerator: UISelectionFeedbackGenerator?
 
-    @objc public func impact(_ style: String?) {
-        DispatchQueue.main.async {
-            if let style = style {
-                var impactStyle = UIImpactFeedbackGenerator.FeedbackStyle.heavy
-                if style == "MEDIUM" {
-                    impactStyle = UIImpactFeedbackGenerator.FeedbackStyle.medium
-                } else if style == "LIGHT" {
-                    impactStyle = UIImpactFeedbackGenerator.FeedbackStyle.light
-                }
-
-                let generator = UIImpactFeedbackGenerator(style: impactStyle)
-                generator.impactOccurred()
-            } else {
-                let generator = UIImpactFeedbackGenerator(style: .heavy)
-                generator.impactOccurred()
-            }
-        }
+    @objc public func impact(_ impactStyle: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: impactStyle)
+        generator.impactOccurred()
     }
 
-    @objc public func notification(_ type: String?) {
-        DispatchQueue.main.async {
-            let generator = UINotificationFeedbackGenerator()
-            if let type = type {
-                var notificationType = UINotificationFeedbackGenerator.FeedbackType.success
-                if type == "WARNING" {
-                    notificationType = UINotificationFeedbackGenerator.FeedbackType.warning
-                } else if type == "ERROR" {
-                    notificationType = UINotificationFeedbackGenerator.FeedbackType.error
-                }
-                generator.notificationOccurred(notificationType)
-            } else {
-                generator.notificationOccurred(.success)
-            }
-        }
+    @objc public func notification(_ notificationType: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(notificationType)
     }
 
     @objc public func selectionStart() {
-        DispatchQueue.main.async {
-            self.selectionFeedbackGenerator = UISelectionFeedbackGenerator()
-            self.selectionFeedbackGenerator?.prepare()
-        }
+        self.selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+        self.selectionFeedbackGenerator?.prepare()
     }
 
     @objc public func selectionChanged() {
-        DispatchQueue.main.async {
-            if let generator = self.selectionFeedbackGenerator {
-                generator.selectionChanged()
-                generator.prepare()
-            }
+        if let generator = self.selectionFeedbackGenerator {
+            generator.selectionChanged()
+            generator.prepare()
         }
     }
 
     @objc public func selectionEnd() {
-        DispatchQueue.main.async {
-            self.selectionFeedbackGenerator = nil
-        }
+        self.selectionFeedbackGenerator = nil
     }
 
     @objc public func vibrate() {
-        DispatchQueue.main.async {
-            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
-        }
+        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
     }
 }
--- haptics/ios/Plugin/HapticsPlugin.swift
+++ haptics/ios/Plugin/HapticsPlugin.swift
@@ -5,32 +5,60 @@ public class HapticsPlugin: CAPPlugin {
     private let implementation = Haptics()
 
     @objc public func impact(_ call: CAPPluginCall) {
-        implementation.impact(call.options["style"] as? String)
+        var impactStyle = UIImpactFeedbackGenerator.FeedbackStyle.heavy
+        if let style = call.options["style"] as? String {
+            if style == "MEDIUM" {
+                impactStyle = UIImpactFeedbackGenerator.FeedbackStyle.medium
+            } else if style == "LIGHT" {
+                impactStyle = UIImpactFeedbackGenerator.FeedbackStyle.light
+            }
+        }
+        DispatchQueue.main.async {
+            self.implementation.impact(impactStyle)
+        }
         call.resolve()
     }
 
     @objc public func notification(_ call: CAPPluginCall) {
-        implementation.notification(call.options["type"] as? String)
+        var notificationType = UINotificationFeedbackGenerator.FeedbackType.success
+        if let type = call.options["type"] as? String {
+            if type == "WARNING" {
+                notificationType = UINotificationFeedbackGenerator.FeedbackType.warning
+            } else if type == "ERROR" {
+                notificationType = UINotificationFeedbackGenerator.FeedbackType.error
+            }
+        }
+        DispatchQueue.main.async {
+            self.implementation.notification(notificationType)
+        }
         call.resolve()
     }
 
     @objc public func selectionStart(_ call: CAPPluginCall) {
-        implementation.selectionStart()
+        DispatchQueue.main.async {
+            self.implementation.selectionStart()
+        }
         call.resolve()
     }
 
     @objc public func selectionChanged(_ call: CAPPluginCall) {
-        implementation.selectionChanged()
+        DispatchQueue.main.async {
+            self.implementation.selectionChanged()
+        }
         call.resolve()
     }
 
     @objc public func selectionEnd(_ call: CAPPluginCall) {
-        implementation.selectionEnd()
+        DispatchQueue.main.async {
+            self.implementation.selectionEnd()
+        }
         call.resolve()
     }
 
     @objc public func vibrate(_ call: CAPPluginCall) {
-        implementation.vibrate()
+        DispatchQueue.main.async {
+            self.implementation.vibrate()
+        }
         call.resolve()
     }
 }
