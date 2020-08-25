27caffc46f603ad50087ba8f69d1ffdd78e2431d
Ian Keith <iankeith@gmail.com>
2020-08-25 12:03:07 -0400

    Updating iOS implementation.

--- browser/ios/Plugin/BrowserPlugin.swift
+++ browser/ios/Plugin/BrowserPlugin.swift
@@ -2,67 +2,52 @@ import Foundation
 import Capacitor
 import SafariServices
 
-//@objc(BrowserPlugin)
-//public class BrowserPlugin: CAPPlugin {
-//    private let implementation = Browser()
-//
-//    @objc func echo(_ call: CAPPluginCall) {
-//        let value = call.getString("value") ?? ""
-//        call.success([
-//            "value": implementation.echo(value)
-//        ])
-//    }
-//}
-
 @objc(CAPBrowserPlugin)
 public class CAPBrowserPlugin: CAPPlugin, SFSafariViewControllerDelegate {
+    private let implementation = Browser()
+    
     var vc: SFSafariViewController?
-
+    
     @objc func open(_ call: CAPPluginCall) {
-        guard let urlString = call.getString("url") else {
-            call.reject("Must provide a URL to open")
+        // validate the URL
+        guard let urlString = call.getString("url"), let url = URL(string: urlString) else {
+            call.reject("Must provide a valid URL to open")
             return
         }
-
-        if urlString.isEmpty {
-            call.reject("URL must not be empty")
+        // extract the optional parameters
+        var color: UIColor? = nil
+        if let toolbarColor = call.getString("toolbarColor") {
+            color = UIColor.capacitor.color(fromHex: toolbarColor)
+        }
+        let style = self.presentationStyle(for: call.getString("presentationStyle"))
+        // prepare for display
+        guard implementation.prepare(for: url, withTint: color, modalPresentation: style), let viewController = implementation.viewController else {
+            call.reject("Unable to display URL")
             return
         }
-
-        let toolbarColor = call.getString("toolbarColor")
-        let url = URL(string: urlString)
-        if let scheme = url?.scheme, ["http", "https"].contains(scheme.lowercased()) {
-            DispatchQueue.main.async { [weak self] in
-                let safariVC = SFSafariViewController.init(url: url!)
-                safariVC.delegate = self
-                let presentationStyle = call.getString("presentationStyle")
-                if presentationStyle != nil && presentationStyle == "popover" && (self?.supportsPopover() ?? false) {
-                    safariVC.modalPresentationStyle = .popover
-                    self?.setCenteredPopover(safariVC)
-                } else {
-                    safariVC.modalPresentationStyle = .fullScreen
+        implementation.browserEventDidOccur = { [weak self] (event) in
+            self?.notifyListeners(event.listenerEvent, data: [:])
+        }
+        // display
+        DispatchQueue.main.async { [weak self] in
+            self?.bridge?.presentVC(viewController, animated: true, completion: {
+                if style == .popover {
+                    self?.setCenteredPopover(viewController)
                 }
-
-                if let toolbarColor = toolbarColor {
-                    safariVC.preferredBarTintColor = UIColor.capacitor.color(fromHex: toolbarColor)
-                }
-                
-                self?.bridge?.presentVC(safariVC, animated: true, completion: {
-                    call.resolve()
-                })
-                self?.vc = safariVC
-            }
-        } else {
-            call.reject("Invalid URL")
+                call.resolve()
+            })
         }
     }
 
     @objc func close(_ call: CAPPluginCall) {
-        if vc == nil {
-            call.resolve()
-        }
-        DispatchQueue.main.async {
-            self.bridge?.dismissVC(animated: true) {
+        DispatchQueue.main.async { [weak self] in
+            if self?.implementation.viewController != nil {
+                self?.bridge?.dismissVC(animated: true) {
+                    call.resolve()
+                    self?.implementation.cleanup()
+                }
+            }
+            else {
                 call.resolve()
             }
         }
@@ -71,13 +56,22 @@ public class CAPBrowserPlugin: CAPPlugin, SFSafariViewControllerDelegate {
     @objc func prefetch(_ call: CAPPluginCall) {
         call.unimplemented()
     }
-
-    public func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        self.notifyListeners("browserFinished", data: [:])
-        vc = nil
+    
+    private func presentationStyle(for style: String?) -> UIModalPresentationStyle {
+        if let style = style, style == "popover", supportsPopover() {
+            return .popover
+        }
+        return .fullScreen
     }
+}
 
-    public func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
-        self.notifyListeners("browserPageLoaded", data: [:])
+private extension BrowserEvent {
+    var listenerEvent: String {
+        switch self {
+        case .loaded:
+            return "browserPageLoaded"
+        case .finished:
+            return "browserFinished"
+        }
     }
 }
