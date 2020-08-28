1b20b049064205316eacb434640c666a015690da
Ian Keith <iankeith@gmail.com>
2020-08-28 15:21:24 -0400

    Android implementation.

--- browser/android/src/main/java/com/capacitorjs/plugins/browser/Browser.java
+++ browser/android/src/main/java/com/capacitorjs/plugins/browser/Browser.java
@@ -1,9 +1,202 @@
 package com.capacitorjs.plugins.browser;
-//
-// public class Browser {
-//
-//     public String echo(String value) {
-//         return value;
-//     }
-// }
-//package com.getcapacitor.plugin;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import java.util.List;
+import androidx.browser.customtabs.CustomTabsCallback;
+import androidx.browser.customtabs.CustomTabsClient;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.browser.customtabs.CustomTabsServiceConnection;
+import androidx.browser.customtabs.CustomTabsSession;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * The Browser class implements Custom Chrome Tabs. See
+ * https://developer.chrome.com/multidevice/android/customtabs for background
+ * on how this code works.
+ */
+public class Browser {
+    /**
+     * Interface for callbacks for browser events.
+     */
+    interface BrowserEventListener {
+        void onBrowserEvent(int event);
+    }
+    /**
+     * Sent when the browser has loaded the initial page.
+     */
+    public static final int BROWSER_LOADED = 1;
+    /**
+     * Sent when the browser is finished.
+     */
+    public static final int BROWSER_FINISHED = 2;
+
+    // private properties
+    @Nullable
+    private BrowserEventListener browserEventListener;
+    private Context context;
+    private static final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome"; // Change when in stable
+    private CustomTabsClient customTabsClient;
+    private CustomTabsSession browserSession;
+    private boolean isInitialLoad = false;
+    private EventGroup group;
+    private CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+
+        @Override
+        public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+            customTabsClient = client;
+            client.warmup(0);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {}
+    };
+
+    /**
+     * Create network browser object.
+     * @param context
+     */
+    public Browser(@NonNull Context context) {
+        this.context = context;
+        this.group = new EventGroup(this::handleGroupCompletion);
+    }
+
+    /**
+     * Set the object to receive callbacks.
+     * @param listener
+     */
+    public void setBrowserEventListener(@Nullable BrowserEventListener listener) {
+        this.browserEventListener = listener;
+    }
+
+    /**
+     * Return the object that is receiving callbacks.
+     * @return listener
+     */
+    @Nullable
+    public BrowserEventListener getBrowserEventListenerListener() {
+        return browserEventListener;
+    }
+
+    /**
+     * Returns whether or not the chrome custom tabs are configured and ready to use.
+     * @return flag
+     */
+    public boolean isPrepared() {
+        return (getCustomTabsSession() != null);
+    }
+
+    /**
+     * Open the browser to the specified URL.
+     * @param url
+     */
+    public void open(Uri url) {
+        open(url, null);
+    }
+
+    /**
+     * Open the browser to the specified URL with the specified toolbar color.
+     * @param url
+     * @param toolbarColor
+     */
+    public void open(Uri url, @Nullable Integer toolbarColor) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(getCustomTabsSession());
+
+        builder.addDefaultShareMenuItem();
+
+        if (toolbarColor != null) {
+            builder.setToolbarColor(toolbarColor.intValue());
+        }
+
+        CustomTabsIntent tabsIntent = builder.build();
+        tabsIntent.intent.putExtra(Intent.EXTRA_REFERRER, Uri.parse(Intent.URI_ANDROID_APP_SCHEME + "//" + context.getPackageName()));
+
+        isInitialLoad = true;
+        group.reset();
+        tabsIntent.launchUrl(context, url);
+    }
+
+    /**
+     * Hint to the browser that the specified URLs might be opened.
+     * @param urls
+     */
+    public void prefetch(List<Uri> urls) {
+        CustomTabsSession session = getCustomTabsSession();
+        if (session != null) {
+            for (Uri url : urls) {
+                session.mayLaunchUrl(url, null, null);
+            }
+        }
+    }
+
+    /**
+     * Bind to the custom tabs service, required to be called in the `onResume` lifecycle event.
+     */
+    public boolean bindService() {
+        boolean result = CustomTabsClient.bindCustomTabsService(context, CUSTOM_TAB_PACKAGE_NAME, connection);
+        group.leave();
+        return result;
+    }
+
+    /**
+     * Unbind the custom tabs service, required to be called in the `onPause` lifecycle event.
+     */
+    public void unbindService() {
+        context.unbindService(connection);
+        group.enter();
+    }
+
+    private void handledNavigationEvent(int navigationEvent) {
+        switch (navigationEvent) {
+            case CustomTabsCallback.NAVIGATION_FINISHED:
+                if (isInitialLoad) {
+                    if (browserEventListener != null) {
+                        browserEventListener.onBrowserEvent(BROWSER_LOADED);
+                    }
+                    isInitialLoad = false;
+                }
+                break;
+            case CustomTabsCallback.TAB_HIDDEN:
+                group.leave();
+                break;
+            case CustomTabsCallback.TAB_SHOWN:
+                group.enter();
+                break;
+        }
+    }
+
+    private void handleGroupCompletion() {
+        // events such as TAB_HIDDEN and onPause can occur for multiple reasons and in
+        // different sequences so there is no single point to fire this. so we rely on the
+        // event group to track when it is safe to assume that the browser is done.
+        if (browserEventListener != null) {
+            browserEventListener.onBrowserEvent(BROWSER_FINISHED);
+        }
+    }
+
+    @Nullable
+    private CustomTabsSession getCustomTabsSession() {
+        if (customTabsClient == null) {
+            return null;
+        }
+
+        if (browserSession == null) {
+            browserSession =
+                    customTabsClient.newSession(
+                            new CustomTabsCallback() {
+
+                                @Override
+                                public void onNavigationEvent(int navigationEvent, Bundle extras) {
+                                    handledNavigationEvent(navigationEvent);
+                                }
+                            }
+                    );
+        }
+
+        return browserSession;
+    }
+ }
--- browser/android/src/main/java/com/capacitorjs/plugins/browser/BrowserPlugin.java
+++ browser/android/src/main/java/com/capacitorjs/plugins/browser/BrowserPlugin.java
@@ -1,46 +1,7 @@
 package com.capacitorjs.plugins.browser;
 
-//import android.content.ComponentName;
-//import android.content.Intent;
-//import android.graphics.Color;
-//import android.net.Uri;
-//import android.os.Bundle;
-//
-//import com.getcapacitor.JSArray;
-//import com.getcapacitor.JSObject;
-//import com.getcapacitor.Logger;
-//import com.getcapacitor.NativePlugin;
-//import com.getcapacitor.Plugin;
-//import com.getcapacitor.PluginCall;
-//import com.getcapacitor.PluginMethod;
-//import com.getcapacitor.PluginRequestCodes;
-//
-//import org.json.JSONException;
-//
-//@NativePlugin(name = "Browser")
-//public class BrowserPlugin extends Plugin {
-//    private Browser implementation = new Browser();
-//
-//    @PluginMethod
-//    public void echo(PluginCall call) {
-//        String value = call.getString("value");
-//
-//        JSObject ret = new JSObject();
-//        ret.put("value", implementation.echo(value));
-//        call.success(ret);
-//    }
-//}
-
-import android.content.ComponentName;
-import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
-import android.os.Bundle;
-import androidx.browser.customtabs.CustomTabsCallback;
-import androidx.browser.customtabs.CustomTabsClient;
-import androidx.browser.customtabs.CustomTabsIntent;
-import androidx.browser.customtabs.CustomTabsServiceConnection;
-import androidx.browser.customtabs.CustomTabsSession;
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Logger;
@@ -50,55 +11,51 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.PluginRequestCodes;
 import org.json.JSONException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * The Browser plugin implements Custom Chrome Tabs. See
- * https://developer.chrome.com/multidevice/android/customtabs for background
- * on how this code works.
- */
 @NativePlugin(name = "Browser", requestCodes = { PluginRequestCodes.BROWSER_OPEN_CHROME_TAB })
 public class BrowserPlugin extends Plugin {
-    public static final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome"; // Change when in stable
+    private Browser implementation;
 
-    private CustomTabsClient customTabsClient;
-    private CustomTabsSession currentSession;
-    private boolean fireFinished = false;
+    @PluginMethod
+    public void load() {
+        implementation = new Browser(getContext());
+        implementation.setBrowserEventListener(this::onBrowserEvent);
+    }
 
     @PluginMethod
     public void open(PluginCall call) {
-        String url = call.getString("url");
-        String toolbarColor = call.getString("toolbarColor");
-
-        if (url == null) {
-            call.error("Must provide a URL to open");
+        // get the URL
+        String urlString = call.getString("url");
+        if (urlString == null) {
+            call.reject("Must provide a URL to open");
             return;
         }
-
-        if (url.isEmpty()) {
-            call.error("URL must not be empty");
+        if (urlString.isEmpty()) {
+            call.reject("URL must not be empty");
             return;
         }
-
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(getCustomTabsSession());
-
-        builder.addDefaultShareMenuItem();
-
-        if (toolbarColor != null) {
-            try {
-                builder.setToolbarColor(Color.parseColor(toolbarColor));
-            } catch (IllegalArgumentException ex) {
-                Logger.error(getLogTag(), "Invalid color provided for toolbarColor. Using default", null);
-            }
-        }
-
-        CustomTabsIntent tabsIntent = builder.build();
-        tabsIntent.intent.putExtra(Intent.EXTRA_REFERRER, Uri.parse(Intent.URI_ANDROID_APP_SCHEME + "//" + getContext().getPackageName()));
+        Uri url;
         try {
-            tabsIntent.launchUrl(getContext(), Uri.parse(url));
-            call.success();
+            url = Uri.parse(urlString);
         } catch (Exception ex) {
-            call.error(ex.getLocalizedMessage());
+            call.reject(ex.getLocalizedMessage());
+            return;
         }
+
+        // get the toolbar color, if provided
+        String colorString = call.getString("toolbarColor");
+        Integer toolbarColor = null;
+        if (colorString != null) try {
+            toolbarColor = Color.parseColor(colorString);
+        } catch (IllegalArgumentException ex) {
+            Logger.error(getLogTag(), "Invalid color provided for toolbarColor. Using default", null);
+        }
+
+        // open the browser and finish
+        implementation.open(url, toolbarColor);
+        call.resolve();
     }
 
     @PluginMethod
@@ -108,47 +65,34 @@ public class BrowserPlugin extends Plugin {
 
     @PluginMethod
     public void prefetch(PluginCall call) {
-        JSArray urls = call.getArray("urls");
-        if (urls == null || urls.length() == 0) {
-            call.error("Must provide an array of URLs to prefetch");
+        JSArray urlStrings = call.getArray("urls");
+        if (urlStrings == null || urlStrings.length() == 0) {
+            call.reject("Must provide an array of URLs to prefetch");
             return;
         }
 
-        CustomTabsSession session = getCustomTabsSession();
-
-        if (session == null) {
-            call.error("Browser session isn't ready yet");
+        if (implementation.isPrepared()) {
+            call.reject("Browser session isn't ready yet");
             return;
         }
 
+        List<Uri> urls = new ArrayList();
         try {
-            for (String url : urls.<String>toList()) {
-                session.mayLaunchUrl(Uri.parse(url), null, null);
+            for (String urlString : urlStrings.<String>toList()) {
+                urls.add(Uri.parse(urlString));
             }
         } catch (JSONException ex) {
-            call.error("Unable to process provided urls list. Ensure each item is a string and valid URL", ex);
+            call.reject("Unable to process provided list of urls. Ensure each item is a string and valid URL", ex);
             return;
         }
+
+        implementation.prefetch(urls);
+        call.resolve();
     }
-
-    CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
-
-        @Override
-        public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
-            customTabsClient = client;
-            client.warmup(0);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {}
-    };
 
     @Override
     protected void handleOnResume() {
-        if (fireFinished) {
-            notifyListeners("browserFinished", new JSObject());
-        }
-        boolean ok = CustomTabsClient.bindCustomTabsService(getContext(), CUSTOM_TAB_PACKAGE_NAME, connection);
+        boolean ok = implementation.bindService();
         if (!ok) {
             Logger.error(getLogTag(), "Error binding to custom tabs service", null);
         }
@@ -156,37 +100,17 @@ public class BrowserPlugin extends Plugin {
 
     @Override
     protected void handleOnPause() {
-        getContext().unbindService(connection);
+        implementation.unbindService();
     }
 
-    public CustomTabsSession getCustomTabsSession() {
-        if (customTabsClient == null) {
-            return null;
+    void onBrowserEvent(int event) {
+        switch (event) {
+            case Browser.BROWSER_LOADED:
+                notifyListeners("browserPageLoaded", new JSObject());
+                break;
+            case Browser.BROWSER_FINISHED:
+                notifyListeners("browserFinished", new JSObject());
+                break;
         }
-
-        if (currentSession == null) {
-            currentSession =
-                customTabsClient.newSession(
-                    new CustomTabsCallback() {
-
-                        @Override
-                        public void onNavigationEvent(int navigationEvent, Bundle extras) {
-                            switch (navigationEvent) {
-                                case NAVIGATION_FINISHED:
-                                    notifyListeners("browserPageLoaded", new JSObject());
-                                    break;
-                                case TAB_HIDDEN:
-                                    fireFinished = true;
-                                    break;
-                                case TAB_SHOWN:
-                                    fireFinished = false;
-                                    break;
-                            }
-                        }
-                    }
-                );
-        }
-
-        return currentSession;
     }
 }
