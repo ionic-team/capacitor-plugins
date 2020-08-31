293827cea05d60096446687c1d912a637d0bd386
Ian Keith <iankeith@gmail.com>
2020-08-31 17:06:11 -0400

    Removing test.

--- browser/ios/PluginTests/PluginTests.swift
+++ browser/ios/PluginTests/PluginTests.swift
@@ -12,15 +12,4 @@ class BrowserTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
-    func testEcho() {
-        // This is an example of a functional test case for a plugin.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-
-        let implementation = Browser()
-        let value = "Hello, World!"
-        let result = implementation.echo(value)
-
-        XCTAssertEqual(value, result)
-    }
 }
