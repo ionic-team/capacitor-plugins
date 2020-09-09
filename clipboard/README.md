fd895448fd7c6d105716b31eb4fe42762328513a
Carl Poole <mail@carlpoole.com>
2020-09-09 14:03:31 -0500

    feat: Clipboard plugin
    Co-authored-by: Ian Keith <iankeith@gmail.com>
    Co-authored-by: jcesarmobile <jcesarmobile@gmail.com>
    Co-authored-by: Dan Imhoff <dwieeb@gmail.com>

---
+++ clipboard/README.md
@@ -0,0 +1,3 @@
+# @capacitor/clipboard
+
+The Clipboard API enables copy and pasting to/from the system clipboard.
---
+++ clipboard/android/src/main/java/com/capacitorjs/plugins/clipboard/ClipboardPlugin.java
@@ -0,0 +1,61 @@
+package com.capacitorjs.plugins.clipboard;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+
+@NativePlugin(name = "Clipboard")
+public class ClipboardPlugin extends Plugin {
+    private Clipboard implementation;
+
+    @Override
+    public void load() {
+        implementation = new Clipboard(getContext());
+    }
+
+    @PluginMethod
+    public void write(PluginCall call) {
+        String strVal = call.getString("string");
+        String imageVal = call.getString("image");
+        String urlVal = call.getString("url");
+        String label = call.getString("label");
+
+        ClipboardWriteResponse response;
+
+        if (strVal != null) {
+            response = implementation.write(label, strVal);
+        } else if (imageVal != null) {
+            response = implementation.write(label, imageVal);
+        } else if (urlVal != null) {
+            response = implementation.write(label, urlVal);
+        } else {
+            call.reject("No data provided");
+            return;
+        }
+
+        if (response.isSuccess()) {
+            call.resolve();
+        } else {
+            call.reject(response.getErrorMessage());
+        }
+    }
+
+    @PluginMethod
+    public void read(PluginCall call) {
+        ClipboardData result = implementation.read();
+
+        if (result == null) {
+            call.reject("Unable to read clipboard from the given Context");
+        } else if (result.getValue() == null) {
+            call.reject("There is no data on the clipboard");
+        } else {
+            JSObject resultJS = new JSObject();
+            resultJS.put("value", result.getValue());
+            resultJS.put("type", result.getType());
+
+            call.resolve(resultJS);
+        }
+    }
+}
