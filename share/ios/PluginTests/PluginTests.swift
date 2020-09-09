d308299c6e8f28c44c84b09bdefe914a771fa04e
jcesarmobile <jcesarmobile@gmail.com>
2020-09-09 19:28:46 +0200

    fix tests class name

--- share/ios/PluginTests/PluginTests.swift
+++ share/ios/PluginTests/PluginTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Plugin
 
-class {{{ CLASS }}}Tests: XCTestCase {
+class ShareTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,7 +16,7 @@ class {{{ CLASS }}}Tests: XCTestCase {
         // This is an example of a functional test case for a plugin.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-        let implementation = {{{ CLASS }}}()
+        let implementation = Share()
         let value = "Hello, World!"
         let result = implementation.echo(value)
 
