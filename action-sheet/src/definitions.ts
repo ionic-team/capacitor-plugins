030995f658e889ed3915602d5358f99e8697a29f
jcesarmobile <jcesarmobile@gmail.com>
2020-09-16 16:06:12 +0200

    better types

--- action-sheet/src/definitions.ts
+++ action-sheet/src/definitions.ts
@@ -5,30 +5,87 @@ declare module '@capacitor/core' {
 }
 
 export interface ActionSheetOptions {
-  title: string;
   /**
-   * iOS only
+   * The title of the Action Sheet.
+   *
+   * @since 1.0.0
+   */
+  title: string;
+
+  /**
+   * A message to show under the title.
+   *
+   * This option is only supported on iOS.
+   *
+   * @since 1.0.0
    */
   message?: string;
+
+  /**
+   * Options the user can choose from.
+   *
+   * @since 1.0.0
+   */
   options: ActionSheetOption[];
 }
 
 export enum ActionSheetOptionStyle {
+  /**
+   * Default style of the option.
+   *
+   * @since 1.0.0
+   */
   Default = 'DEFAULT',
+
+  /**
+   * Style to use on destructive options.
+   *
+   * @since 1.0.0
+   */
   Destructive = 'DESTRUCTIVE',
+
+  /**
+   * Style to use on the option that cancels the Action Sheet.
+   * If used, should be on the latest availabe option.
+   *
+   * @since 1.0.0
+   */
   Cancel = 'CANCEL',
 }
 
 export interface ActionSheetOption {
-  title: string;
-  style?: ActionSheetOptionStyle;
   /**
-   * Icon for web (ionicon naming convention)
+   * The title of the option
+   *
+   * @since 1.0.0
+   */
+  title: string;
+
+  /**
+   * The style of the option
+   *
+   * This option is only supported on iOS.
+   *
+   * @since 1.0.0
+   */
+  style?: ActionSheetOptionStyle;
+
+  /**
+   * Icon for the option (ionicon naming convention)
+   *
+   * This option is only supported on Web.
+   *
+   * @since 1.0.0
    */
   icon?: string;
 }
 
 export interface ActionSheetResult {
+  /**
+   * The index of the clicked option (Zero-based)
+   *
+   * @since 1.0.0
+   */
   index: number;
 }
 
@@ -36,6 +93,8 @@ export interface ActionSheetPlugin {
   /**
    * Show an Action Sheet style modal with various options for the user
    * to select.
+   *
+   * @since 1.0.0
    */
   showActions(options: ActionSheetOptions): Promise<ActionSheetResult>;
 }
