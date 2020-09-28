1c0bcc163e3c6ef882819d287c1c61dcfb1e2b33
jcesarmobile <jcesarmobile@gmail.com>
2020-09-28 17:21:18 +0200

    Update toast/ios/Plugin/ToastLabel.swift
    Co-authored-by: Ian Keith <iankeith@gmail.com>

--- toast/ios/Plugin/ToastLabel.swift
+++ toast/ios/Plugin/ToastLabel.swift
@@ -1,20 +1,7 @@
 import UIKit
 
 @objc public class ToastLabel: UILabel {
-    private struct AssociatedKeys {
-        static var padding = UIEdgeInsets()
-    }
-
-    public var padding: UIEdgeInsets? {
-        get {
-            return objc_getAssociatedObject(self, &AssociatedKeys.padding) as? UIEdgeInsets
-        }
-        set {
-            if let newValue = newValue {
-                objc_setAssociatedObject(self, &AssociatedKeys.padding, newValue as UIEdgeInsets, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-            }
-        }
-    }
+    var padding: UIEdgeInsets?
 
     override open func draw(_ rect: CGRect) {
         if let insets = padding {
