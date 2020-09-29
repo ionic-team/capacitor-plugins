a3588195ddc08460c320b2d96036572be40c481a
jcesarmobile <jcesarmobile@gmail.com>
2020-09-29 13:46:57 +0200

    Update filesystem/ios/Plugin/FilesystemPlugin.swift
    Co-authored-by: Ian Keith <iankeith@gmail.com>

--- filesystem/ios/Plugin/FilesystemPlugin.swift
+++ filesystem/ios/Plugin/FilesystemPlugin.swift
@@ -53,7 +53,7 @@ public class FilesystemPlugin: CAPPlugin {
             return
         }
 
-        let directory = call.get("directory", String.self) ?? defaultDirectory
+        let directory = call.getString("directory") ?? defaultDirectory
 
         guard let fileUrl = implementation.getFileUrl(at: file, in: directory) else {
             handleError(call, "Invalid path")
