ee8c088fa593125280696477800ef0de1419761d
jcesarmobile <jcesarmobile@gmail.com>
2020-09-29 14:01:10 +0200

    Update filesystem/ios/Plugin/FilesystemPlugin.swift
    Co-authored-by: Ian Keith <iankeith@gmail.com>

--- filesystem/ios/Plugin/FilesystemPlugin.swift
+++ filesystem/ios/Plugin/FilesystemPlugin.swift
@@ -178,7 +178,7 @@ public class FilesystemPlugin: CAPPlugin {
      * Read the contents of a directory.
      */
     @objc func readdir(_ call: CAPPluginCall) {
-        guard let path = call.get("path", String.self) else {
+        guard let path = call.getString("path") else {
             handleError(call, "path must be provided and must be a string.")
             return
         }
