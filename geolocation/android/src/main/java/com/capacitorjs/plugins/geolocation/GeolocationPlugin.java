228b6ce391593b8dc319b2664dbf9ee9d3ab6e36
Carl Poole <carl@ionic.io>
2020-10-09 16:45:19 -0500

    Updated to work with new permissions changes WIP, some cleanup

--- geolocation/android/src/main/java/com/capacitorjs/plugins/geolocation/GeolocationPlugin.java
+++ geolocation/android/src/main/java/com/capacitorjs/plugins/geolocation/GeolocationPlugin.java
@@ -2,28 +2,27 @@ package com.capacitorjs.plugins.geolocation;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import com.getcapacitor.CapacitorPlugin;
 import com.getcapacitor.JSObject;
-import com.getcapacitor.Permission;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.PluginRequestCodes;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
 import java.util.HashMap;
 import java.util.Map;
 
 @CapacitorPlugin(
     name = "Geolocation",
     permissions = {
-        @Permission(permission = Manifest.permission.ACCESS_COARSE_LOCATION, alias = "networkLocation"),
-        @Permission(permission = Manifest.permission.ACCESS_FINE_LOCATION, alias = "gps")
+        @Permission(permission = Manifest.permission.ACCESS_COARSE_LOCATION, alias = "location"),
+        @Permission(permission = Manifest.permission.ACCESS_FINE_LOCATION, alias = "location")
     },
-    permissionRequestCode = PluginRequestCodes.GEOLOCATION_REQUEST_PERMISSIONS
+    permissionRequestCode = GeolocationPlugin.GEOLOCATION_REQUEST_PERMISSIONS
 )
 public class GeolocationPlugin extends Plugin {
-    private Geolocation implementation;
+    public static final int GEOLOCATION_REQUEST_PERMISSIONS = 9004;
 
+    private Geolocation implementation;
     private Map<String, PluginCall> watchingCalls = new HashMap<>();
 
     @Override
@@ -31,6 +30,52 @@ public class GeolocationPlugin extends Plugin {
         implementation = new Geolocation(getContext());
     }
 
+    /**
+     * Gets a snapshot of the current device position if permissions are granted. If not,
+     * the call is rejected in {@link #onRequestPermissionsResult(int, String[], int[])}.
+     *
+     * @param call Plugin call
+     */
+    @PluginMethod
+    public void getCurrentPosition(final PluginCall call) {
+        if (!hasRequiredPermissions()) {
+            saveCall(call);
+            pluginRequestAllPermissions();
+        } else {
+            getPosition(call);
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getPosition(PluginCall call) {
+        boolean enableHighAccuracy = call.getBoolean("enableHighAccuracy", false);
+        int timeout = call.getInt("timeout", 10000);
+
+        implementation.sendLocation(
+            enableHighAccuracy,
+            timeout,
+            false,
+            new LocationResultCallback() {
+
+                @Override
+                public void success(JSObject location) {
+                    call.resolve(location);
+                }
+
+                @Override
+                public void error(String message) {
+                    call.reject(message);
+                }
+            }
+        );
+    }
+
+    /**
+     * Begins watching for live location changes if permissions are granted. If not,
+     * the call is rejected in {@link #onRequestPermissionsResult(int, String[], int[])}.
+     *
+     * @param call Plugin call
+     */
     @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
     public void watchPosition(PluginCall call) {
         call.save();
@@ -39,35 +84,6 @@ public class GeolocationPlugin extends Plugin {
             pluginRequestAllPermissions();
         } else {
             startWatch(call);
-        }
-    }
-
-    @PluginMethod
-    public void getCurrentPosition(final PluginCall call) {
-        if (!hasRequiredPermissions()) {
-            saveCall(call);
-            pluginRequestAllPermissions();
-        } else {
-            boolean enableHighAccuracy = call.getBoolean("enableHighAccuracy", false);
-            int timeout = call.getInt("timeout", 10000);
-
-            implementation.sendLocation(
-                enableHighAccuracy,
-                timeout,
-                false,
-                new LocationResultCallback() {
-
-                    @Override
-                    public void success(JSObject location) {
-                        call.resolve(location);
-                    }
-
-                    @Override
-                    public void error(String message) {
-                        call.reject(message);
-                    }
-                }
-            );
         }
     }
 
@@ -97,6 +113,11 @@ public class GeolocationPlugin extends Plugin {
         watchingCalls.put(call.getCallbackId(), call);
     }
 
+    /**
+     * Removes an active geolocation watch.
+     *
+     * @param call Plugin call
+     */
     @SuppressWarnings("MissingPermission")
     @PluginMethod
     public void clearWatch(PluginCall call) {
@@ -113,48 +134,71 @@ public class GeolocationPlugin extends Plugin {
         call.resolve();
     }
 
+    /**
+     * Handle permission results related to the plugin. Permission calls may happen by direct
+     * plugin call or during a call to {@link #getCurrentPosition} or {@link #watchPosition}.
+     *
+     * The result of a direct call will return the result states for each permission.
+     *
+     * @param requestCode The code associated with the permission request {@link #GEOLOCATION_REQUEST_PERMISSIONS}
+     * @param permissions The permissions requested
+     * @param grantResults The result status
+     */
     @Override
-    protected void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.handleRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        final PluginCall savedCall = getSavedCall();
+    protected void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        PluginCall savedCall = getSavedCall();
         if (savedCall == null) {
             return;
         }
 
-        for (int result : grantResults) {
-            if (result == PackageManager.PERMISSION_DENIED) {
-                savedCall.reject("User denied location permission");
-                return;
-            }
-        }
-
         if (savedCall.getMethodName().equals("getCurrentPosition")) {
-            boolean enableHighAccuracy = savedCall.getBoolean("enableHighAccuracy", false);
-            int timeout = savedCall.getInt("timeout", 10000);
+            if (!handleDenied(savedCall, grantResults)) {
+                boolean enableHighAccuracy = savedCall.getBoolean("enableHighAccuracy", false);
+                int timeout = savedCall.getInt("timeout", 10000);
 
-            implementation.sendLocation(
-                enableHighAccuracy,
-                timeout,
-                true,
-                new LocationResultCallback() {
+                implementation.sendLocation(
+                    enableHighAccuracy,
+                    timeout,
+                    true,
+                    new LocationResultCallback() {
 
-                    @Override
-                    public void success(JSObject location) {
-                        savedCall.resolve(location);
+                        @Override
+                        public void success(JSObject location) {
+                            savedCall.resolve(location);
+                        }
+
+                        @Override
+                        public void error(String message) {
+                            savedCall.reject(message);
+                        }
                     }
-
-                    @Override
-                    public void error(String message) {
-                        savedCall.reject(message);
-                    }
-                }
-            );
+                );
+            }
         } else if (savedCall.getMethodName().equals("watchPosition")) {
-            startWatch(savedCall);
+            if (!handleDenied(savedCall, grantResults)) {
+                startWatch(savedCall);
+            }
         } else {
             savedCall.resolve(getPermissionStates());
             savedCall.release(bridge);
         }
+    }
+
+    /**
+     * Return rejection if permissions were denied during a request triggered from a plugin call.
+     *
+     * @param savedCall The cached plugin call to respond to
+     * @param grantResults The plugin permission results
+     * @return true if a denied permission occurred and the rejection is handled, false if not
+     */
+    private boolean handleDenied(PluginCall savedCall, int[] grantResults) {
+        for (int result : grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED) {
+                savedCall.reject("User denied location permission");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
