37dc8ab8e51419525c154fd4a24e48def0db5f5f
jcesarmobile <jcesarmobile@gmail.com>
2020-10-19 15:30:04 +0200

    fmt

--- app/android/src/main/java/com/capacitorjs/plugins/app/AppPlugin.java
+++ app/android/src/main/java/com/capacitorjs/plugins/app/AppPlugin.java
@@ -10,6 +10,7 @@ import com.getcapacitor.*;
 
 @NativePlugin(name = "App")
 public class AppPlugin extends Plugin {
+
     private static final String EVENT_BACK_BUTTON = "backButton";
     private static final String EVENT_URL_OPEN = "appUrlOpen";
     private static final String EVENT_STATE_CHANGE = "appStateChange";
