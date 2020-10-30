0b666ffd539ef11e622422880fc1820853bf6323
Dan Imhoff <dwieeb@gmail.com>
2020-10-30 13:42:23 -0700

    chore: update plugins to 3.0.0-alpha.6 (#84)
    Co-authored-by: jcesarmobile <jcesarmobile@gmail.com>

--- share/src/web.ts
+++ share/src/web.ts
@@ -1,4 +1,4 @@
-import { UnsupportedBrowserException, WebPlugin } from '@capacitor/core';
+import { WebPlugin } from '@capacitor/core';
 
 import type { ShareOptions, SharePlugin, ShareResult } from './definitions';
 
@@ -9,9 +9,7 @@ export class ShareWeb extends WebPlugin implements SharePlugin {
 
   async share(options: ShareOptions): Promise<ShareResult> {
     if (typeof navigator === 'undefined' || !navigator.share) {
-      throw new UnsupportedBrowserException(
-        'Share API not available in this browser',
-      );
+      throw this.unavailable('Share API not available in this browser');
     }
 
     await navigator.share({
--- toast/ios/Plugin/ToastPlugin.swift
+++ toast/ios/Plugin/ToastPlugin.swift
@@ -5,13 +5,13 @@ import Capacitor
 public class ToastPlugin: CAPPlugin {
 
     @objc func show(_ call: CAPPluginCall) {
-        guard let text = call.get("text", String.self) else {
+        guard let text = call.getString("text") else {
             call.reject("text must be provided and must be a string.")
             return
         }
-        let durationType = call.get("duration", String.self, "short")!
+        let durationType = call.getString("duration", "short")
         let duration = durationType == "long" ? 3500 : 2000
-        let position = call.get("position", String.self, "bottom")!
+        let position = call.getString("position", "bottom")
 
         guard let viewController = bridge?.viewController else {
             call.reject("Unable to display toast!")
