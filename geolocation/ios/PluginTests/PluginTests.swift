27d17b7403e2313f15f9df84754c8cf3b6b06f4e
Carl Poole <mail@carlpoole.com>
2020-11-03 10:59:39 -0600

    remove example test
    Co-authored-by: jcesarmobile <jcesarmobile@gmail.com>

--- geolocation/ios/PluginTests/PluginTests.swift
+++ geolocation/ios/PluginTests/PluginTests.swift
@@ -13,23 +13,4 @@ class PluginTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
-    func testEcho() {
-        // This is an example of a functional test case for a plugin.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-
-        let value = "Hello, World!"
-        let plugin = MyPlugin()
-
-        let call = CAPPluginCall(callbackId: "test", options: [
-            "value": value
-        ], success: { (result, _) in
-            let resultValue = result!.data["value"] as? String
-            XCTAssertEqual(value, resultValue)
-        }, error: { (_) in
-            XCTFail("Error shouldn't have been called")
-        })
-
-        plugin.echo(call!)
-    }
 }
