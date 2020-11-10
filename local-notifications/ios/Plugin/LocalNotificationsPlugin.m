712aaa3cf84ea2cb949ab00b2eb2b034f8e00330
Daniel Imhoff <dwieeb@gmail.com>
2020-11-09 16:37:55 -0800

    import ios code

--- local-notifications/ios/Plugin/LocalNotificationsPlugin.m
+++ local-notifications/ios/Plugin/LocalNotificationsPlugin.m
@@ -1,8 +1,16 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(LocalNotificationsPlugin, "LocalNotifications",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(schedule, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(cancel, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(getPending, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(registerActionTypes, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(areEnabled, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(createChannel, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(deleteChannel, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(listChannels, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnNone);
 )
