d34c9ca36e50cef9aaca3e7c454a2844894a82f0
Ian Keith <iankeith@gmail.com>
2020-11-17 18:02:01 -0500

    Export iOS methods

--- camera/ios/Plugin/CameraPlugin.m
+++ camera/ios/Plugin/CameraPlugin.m
@@ -3,4 +3,6 @@
 
 CAP_PLUGIN(CAPCameraPlugin, "Camera",
   CAP_PLUGIN_METHOD(getPhoto, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
 )
