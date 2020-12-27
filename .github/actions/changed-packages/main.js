72493c7021c3b6ba396ce92a474cf3b2efa306bd
Daniel Imhoff <dwieeb@gmail.com>
2020-12-26 16:40:04 -0800

    wip

--- .github/actions/changed-packages/main.js
+++ .github/actions/changed-packages/main.js
@@ -1,12 +1,18 @@
 require = require('esm')(module);
 
+const { resolve } = require('path');
 const core = require('@actions/core')
+
+const { root } = require('../../../scripts/lib/repo.mjs');
 const { execute } = require('../../../scripts/lib/cli.mjs');
 const { ls } = require('../../../scripts/lib/lerna.mjs');
 
 execute(async () => {
-  const changedFiles = core.getInput('changed-files', []);
+  const changedFiles = JSON.parse(core.getInput('changed-files', '[]')).map(f => resolve(root, f));
   const packages = await ls();
+  const changedPackages = packages.filter(pkg => changedFiles.some(f => f.startsWith(pkg.location)));
+  const changedNames = changedPackages.map(pkg => pkg.name);
 
-  console.log(changedFiles, packages);
+  core.info(`Changed packages: ${changedNames}`);
+  core.setOutput('changed-packages', changedNames);
 });
