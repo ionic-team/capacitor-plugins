b28993d133a82f534af63362247a2b17784f98b4
Daniel Imhoff <dwieeb@gmail.com>
2021-01-08 16:34:12 -0800

    add docs for cli hints

--- push-notifications/src/definitions.ts
+++ push-notifications/src/definitions.ts
@@ -1,4 +1,30 @@
+/// <reference types="@capacitor/cli" />
+
 import type { PermissionState, PluginListenerHandle } from '@capacitor/core';
+
+export type PresentationOption = 'badge' | 'sound' | 'alert';
+
+declare module '@capacitor/cli' {
+  export interface PluginsConfig {
+    PushNotifications?: {
+      /**
+       * Configure the way push notifications are displayed when the app is in foreground.
+       *
+       * This is an array of strings you can combine. Possible values in the array are:
+       *   - `badge`: badge count on the app icon is updated (default value)
+       *   - `sound`: the device will ring/vibrate when the push notification is received
+       *   - `alert`: the push notification is displayed in a native dialog
+       *
+       * An empty array can be provided if none of the options are desired.
+       *
+       * Only available for iOS.
+       *
+       * @since 1.0.0
+       */
+      presentationOptions: PresentationOption[];
+    };
+  }
+}
 
 export interface PushNotificationsPlugin {
   /**
