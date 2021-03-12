458035deb4440ea88fde1f23b28612891b88cfa9
jcesarmobile <jcesarmobile@gmail.com>
2021-03-12 19:48:07 +0100

    fix(local-notifications): don't store notifications if not scheduled

--- local-notifications/android/src/main/java/com/capacitorjs/plugins/localnotifications/NotificationStorage.java
+++ local-notifications/android/src/main/java/com/capacitorjs/plugins/localnotifications/NotificationStorage.java
@@ -36,8 +36,10 @@ public class NotificationStorage {
         SharedPreferences storage = getStorage(NOTIFICATION_STORE_ID);
         SharedPreferences.Editor editor = storage.edit();
         for (LocalNotification request : localNotifications) {
-            String key = request.getId().toString();
-            editor.putString(key, request.getSource());
+            if (request.isScheduled()) {
+                String key = request.getId().toString();
+                editor.putString(key, request.getSource());
+            }
         }
         editor.apply();
     }
