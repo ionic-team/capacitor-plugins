735e3ba22f16f7f4451cd23642e7c1b454a848af
jcesarmobile <jcesarmobile@gmail.com>
2021-06-17 15:38:22 +0200

    fix(camera): Make input file hidden

--- camera/src/web.ts
+++ camera/src/web.ts
@@ -62,6 +62,7 @@ export class CameraWeb extends WebPlugin implements CameraPlugin {
       input = document.createElement('input') as HTMLInputElement;
       input.id = '_capacitor-camera-input';
       input.type = 'file';
+      input.hidden = true;
       document.body.appendChild(input);
     }
 
