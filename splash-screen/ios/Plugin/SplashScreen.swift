641a907ec59fd5195e0dd4675ad022ec53ca74b1
Rishat Shamsutdinov <rishat.sh91@gmail.com>
2021-09-24 17:06:34 +0500

    Fix issue with scenes on iOS 13+
    On iOS 13 there is scenes (`UIScene`). When you're using scenes, the `window` property of the `UIApplicationDelegate` is `nil`.

--- splash-screen/ios/Plugin/SplashScreen.swift
+++ splash-screen/ios/Plugin/SplashScreen.swift
@@ -117,16 +117,17 @@ import Capacitor
     // Update the bounds for the splash image. This will also be called when
     // the parent view observers fire
     private func updateSplashImageBounds() {
-        guard let delegate = UIApplication.shared.delegate else {
-            CAPLog.print("Unable to find root window object for SplashScreen bounds. Please file an issue")
-            return
+        var window: UIWindow? = UIApplication.shared.delegate?.window as? UIWindow
+
+        if #available(iOS 13, *), window == nil {
+            window = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first
         }
 
-        guard let window = delegate.window as? UIWindow else {
+        if let _ = window {
+            viewController.view.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: window!.bounds.size)
+        } else {
             CAPLog.print("Unable to find root window object for SplashScreen bounds. Please file an issue")
-            return
         }
-        viewController.view.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: window.bounds.size)
     }
 
     override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change _: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
