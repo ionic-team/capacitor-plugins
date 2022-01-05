8b85b77309b7fb41ba9b689e4552816f9cc9691a
Chace Daniels <chace@chaces-mbp.lan>
2022-01-05 11:20:08 -0800

    Fixed gradle configuration for android unit tests

--- google-maps/unit-tests/android/app/src/test/java/io/ionic/googlemapstest/GoogleMapsUnitTests.kt
+++ google-maps/unit-tests/android/app/src/test/java/io/ionic/googlemapstest/GoogleMapsUnitTests.kt
@@ -1,23 +1,13 @@
 package io.ionic.googlemapstest
 
-import androidx.appcompat.app.AppCompatActivity
 import com.capacitorjs.plugins.googlemaps.CapacitorGoogleMaps
-import org.junit.Assert.*
-import org.junit.Before
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class GoogleMapsUnitTests {
-    lateinit var activity: AppCompatActivity;
-
-    @Before
-    fun setUp() {
-        activity = Robolectric.buildActivity(MainActivity::class.javaObjectType).create().get()
-    }
-
     @Test
     fun exampleTest() {
         val plugin = CapacitorGoogleMaps()
