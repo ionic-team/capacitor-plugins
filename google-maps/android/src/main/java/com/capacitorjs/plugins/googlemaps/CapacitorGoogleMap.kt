6cfcb0f76235ab8c307f0d34c1f4d3e3d626bdb4
Joseph Pender <joey@ionic.io>
2022-02-08 16:51:31 -0600

    Using coroutines for UI operations

--- google-maps/android/src/main/java/com/capacitorjs/plugins/googlemaps/CapacitorGoogleMap.kt
+++ google-maps/android/src/main/java/com/capacitorjs/plugins/googlemaps/CapacitorGoogleMap.kt
@@ -10,54 +10,77 @@ import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.OnMapReadyCallback
 import com.google.android.libraries.maps.model.Marker
 import com.google.android.libraries.maps.model.MarkerOptions
+import kotlinx.coroutines.channels.Channel
 
 class CapacitorGoogleMap(val id: String, val config: GoogleMapConfig,
                          val delegate: CapacitorGoogleMapsPlugin) : OnMapReadyCallback {
-    var mapView: MapView? = null
+    private var mapView: MapView? = null
     private var googleMap: GoogleMap? = null
     private val markers = HashMap<String, Marker>()
 
+    private val isReadyChannel = Channel<Boolean>()
+
     init {
+        initMap()
+    }
+
+    private fun initMap() {
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                val bridge = delegate.bridge
+
+                val mapView = MapView(bridge.context, this@CapacitorGoogleMap.config.googleMapOptions)
+                mapView.onCreate(null)
+                mapView.onStart()
+                mapView.getMapAsync(this@CapacitorGoogleMap)
+
+                isReadyChannel.receive()
+
+                this@CapacitorGoogleMap.mapView = mapView
+            }
+        }
+
         render()
     }
 
     private fun render() {
-        val bridge = delegate.bridge
-        bridge.activity.runOnUiThread {
-            val mapViewParent = FrameLayout(bridge.context)
-            val layoutParams =
-                FrameLayout.LayoutParams(
-                    getScaledPixels(bridge, this.config.width),
-                    getScaledPixels(bridge, this.config.height),
-                )
-            layoutParams.leftMargin = getScaledPixels(bridge, this.config.x)
-            layoutParams.topMargin = getScaledPixels(bridge, this.config.y)
+        this.mapView ?: throw GoogleMapsError("map view is not available")
 
-            val mapView = MapView(bridge.context, this.config.googleMapOptions)
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                val bridge = delegate.bridge
 
-            mapViewParent.tag = this.id
-            mapView.layoutParams = layoutParams
-            mapViewParent.addView(mapView)
+                val mapViewParent = FrameLayout(bridge.context)
+                val layoutParams =
+                        FrameLayout.LayoutParams(
+                                getScaledPixels(bridge, this@CapacitorGoogleMap.config.width),
+                                getScaledPixels(bridge, this@CapacitorGoogleMap.config.height),
+                        )
+                layoutParams.leftMargin = getScaledPixels(bridge, this@CapacitorGoogleMap.config.x)
+                layoutParams.topMargin = getScaledPixels(bridge, this@CapacitorGoogleMap.config.y)
 
-            ((bridge.webView.parent) as ViewGroup).addView(mapViewParent)
+                mapViewParent.tag = this@CapacitorGoogleMap.id
+                this@CapacitorGoogleMap.mapView!!.layoutParams = layoutParams
+                mapViewParent.addView(mapView)
 
-            this.mapView = mapView
-
-            mapView.onCreate(null)
-            mapView.onStart()
-            mapView.getMapAsync(this)
+                ((bridge.webView.parent) as ViewGroup).addView(mapViewParent)
+            }
         }
     }
 
     fun destroy() {
-        val bridge = delegate.bridge
-        bridge.activity.runOnUiThread {
-            val viewToRemove: View? = ((bridge.webView.parent) as ViewGroup).findViewWithTag(this.id)
-            if (null != viewToRemove) {
-                ((bridge.webView.parent) as ViewGroup).removeView(viewToRemove)
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                val bridge = delegate.bridge
+
+                val viewToRemove: View? = ((bridge.webView.parent) as ViewGroup).findViewWithTag(this@CapacitorGoogleMap.id)
+                if (null != viewToRemove) {
+                    ((bridge.webView.parent) as ViewGroup).removeView(viewToRemove)
+                }
+                this@CapacitorGoogleMap.mapView?.onDestroy()
+                this@CapacitorGoogleMap.mapView = null
+                this@CapacitorGoogleMap.googleMap = null
             }
-            mapView?.onDestroy()
-            mapView = null
         }
     }
 
@@ -67,31 +90,23 @@ class CapacitorGoogleMap(val id: String, val config: GoogleMapConfig,
         var markerId = ""
 
         runBlocking {
-            markerId = uiAddMarker(marker)
+            withContext(Dispatchers.Main) {
+                val markerOptions = MarkerOptions()
+                markerOptions.position(marker.coordinate)
+                markerOptions.title(marker.title)
+                markerOptions.snippet(marker.snippet)
+                markerOptions.alpha(marker.opacity)
+                markerOptions.flat(marker.isFlat)
+                markerOptions.draggable(marker.draggable)
+
+                val newMarker = googleMap!!.addMarker(markerOptions)
+                markers[newMarker.id] = newMarker
+
+                markerId = newMarker.id
+            }
         }
-        
+
         return markerId
-    }
-
-    private suspend fun uiAddMarker(markerConfig: CapacitorGoogleMapMarker): String {
-        var newMarkerId = ""
-
-        withContext(Dispatchers.Main) {
-            val markerOptions = MarkerOptions()
-            markerOptions.position(markerConfig.coordinate)
-            markerOptions.title(markerConfig.title)
-            markerOptions.snippet(markerConfig.snippet)
-            markerOptions.alpha(markerConfig.opacity)
-            markerOptions.flat(markerConfig.isFlat)
-            markerOptions.draggable(markerConfig.draggable)
-
-            val newMarker = googleMap!!.addMarker(markerOptions)
-            markers[newMarker.id] = newMarker
-
-            newMarkerId = newMarker.id
-        }
-
-        return newMarkerId
     }
 
     fun removeMarker(id: String) {
@@ -100,16 +115,12 @@ class CapacitorGoogleMap(val id: String, val config: GoogleMapConfig,
         val marker = markers[id]
         marker?.let {
             runBlocking {
-                uiRemoveMarker(marker)
+                withContext(Dispatchers.Main) {
+                    marker.remove()
+                    markers.remove(marker.id)
+                }
             }
         }
-    }
-
-    private suspend fun uiRemoveMarker(marker: Marker) {
-       withContext(Dispatchers.Main) {
-           marker.remove()
-           markers.remove(marker.id)
-       }
     }
 
     private fun getScaledPixels(bridge: Bridge, pixels: Int): Int {
@@ -120,6 +131,15 @@ class CapacitorGoogleMap(val id: String, val config: GoogleMapConfig,
     }
 
     override fun onMapReady(map: GoogleMap?) {
-        googleMap = map
+        runBlocking {
+            if(map != null) {
+                googleMap = map
+                this@CapacitorGoogleMap.isReadyChannel.send(true);
+                this@CapacitorGoogleMap.isReadyChannel.close()
+            } else {
+                this@CapacitorGoogleMap.isReadyChannel.send(false);
+                this@CapacitorGoogleMap.isReadyChannel.close()
+            }
+        }
     }
 }
