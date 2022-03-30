1e4b2446d0e6b7e2ce87914ebb47ebb3eb336830
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-03-30 13:15:13 -0400

    Update StatusBar.java

--- status-bar/android/src/main/java/com/capacitorjs/plugins/statusbar/StatusBar.java
+++ status-bar/android/src/main/java/com/capacitorjs/plugins/statusbar/StatusBar.java
@@ -6,15 +6,15 @@ import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 
 public class StatusBar {
 
     private int currentStatusbarColor;
     private AppCompatActivity activity;
     private String defaultStyle;
-    private boolean isOverlayed = false;
 
     public StatusBar(AppCompatActivity activity) {
         // save initial color of the status bar
@@ -31,8 +31,12 @@ public class StatusBar {
             style = this.defaultStyle;
         }
 
-        ViewCompat.getWindowInsetsController(decorView).setAppearanceLightStatusBars(!style.equals("DARK"));
+        WindowInsetsControllerCompat windowInsetsControllerCompat = ViewCompat.getWindowInsetsController(decorView);
+        if (windowInsetsControllerCompat != null) {
+            windowInsetsControllerCompat.setAppearanceLightStatusBars(!style.equals("DARK"));
+        }
     }
+
 
     @SuppressWarnings("deprecation")
     public void setBackgroundColor(int color) {
@@ -46,35 +50,52 @@ public class StatusBar {
 
     public void hide() {
         View decorView = activity.getWindow().getDecorView();
-        ViewCompat.getWindowInsetsController(decorView).hide(WindowInsetsCompat.Type.statusBars());
+        WindowInsetsControllerCompat windowInsetsControllerCompat = ViewCompat.getWindowInsetsController(decorView);
+        if (windowInsetsControllerCompat != null) {
+            windowInsetsControllerCompat.hide(WindowInsetsCompat.Type.statusBars());
+        }
     }
 
     public void show() {
         View decorView = activity.getWindow().getDecorView();
-        ViewCompat.getWindowInsetsController(decorView).show(WindowInsetsCompat.Type.statusBars());
+        WindowInsetsControllerCompat windowInsetsControllerCompat = ViewCompat.getWindowInsetsController(decorView);
+        if (windowInsetsControllerCompat != null) {
+            windowInsetsControllerCompat.show(WindowInsetsCompat.Type.statusBars());
+        }
     }
 
+    @SuppressWarnings("deprecation")
     public void setOverlaysWebView(Boolean overlays) {
-        Window window = activity.getWindow();
+        View decorView = activity.getWindow().getDecorView();
+        int uiOptions = decorView.getSystemUiVisibility();
         if (overlays) {
             // Sets the layout to a fullscreen one that does not hide the actual status bar, so the webview is displayed behind it.
-            WindowCompat.setDecorFitsSystemWindows(window, false);
-            currentStatusbarColor = window.getStatusBarColor();
-            window.setStatusBarColor(Color.TRANSPARENT);
-            isOverlayed = true;
+            uiOptions = uiOptions | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            currentStatusbarColor = activity.getWindow().getStatusBarColor();
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
         } else {
-            WindowCompat.setDecorFitsSystemWindows(window, true);
-            window.setStatusBarColor(currentStatusbarColor);
-            isOverlayed = false;
+            // Sets the layout to a normal one that displays the webview below the status bar.
+            uiOptions = uiOptions & ~View.SYSTEM_UI_FLAG_LAYOUT_STABLE & ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // recover the previous color of the status bar
+            activity.getWindow().setStatusBarColor(currentStatusbarColor);
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    private boolean getIsOverlayed() {
+        return (activity.getWindow().getDecorView().getSystemUiVisibility() & View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN) == View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
     }
 
     public StatusBarInfo getInfo() {
         Window window = activity.getWindow();
+        WindowInsetsCompat windowInsetsCompat = ViewCompat.getRootWindowInsets(window.getDecorView());
+        boolean isVisible = windowInsetsCompat != null && windowInsetsCompat.isVisible(WindowInsetsCompat.Type.statusBars());
         StatusBarInfo info = new StatusBarInfo();
         info.setStyle(getStyle());
-        info.setOverlays(isOverlayed);
-        info.setVisible(ViewCompat.getRootWindowInsets(window.getDecorView()).isVisible(WindowInsetsCompat.Type.statusBars()));
+        info.setOverlays(getIsOverlayed());
+        info.setVisible(isVisible);
         info.setColor(String.format("#%06X", (0xFFFFFF & window.getStatusBarColor())));
         return info;
     }
@@ -82,7 +103,8 @@ public class StatusBar {
     private String getStyle() {
         View decorView = activity.getWindow().getDecorView();
         String style;
-        if (ViewCompat.getWindowInsetsController(decorView).isAppearanceLightStatusBars()) {
+        WindowInsetsControllerCompat windowInsetsControllerCompat = ViewCompat.getWindowInsetsController(decorView);
+        if (windowInsetsControllerCompat != null && windowInsetsControllerCompat.isAppearanceLightStatusBars()) {
             style = "LIGHT";
         } else {
             style = "DARK";
