41dbc5456453fe89ecee794164f25cef7c8eefaf
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-04-05 11:51:11 -0400

    chore: fmt

--- status-bar/android/src/main/java/com/capacitorjs/plugins/statusbar/StatusBar.java
+++ status-bar/android/src/main/java/com/capacitorjs/plugins/statusbar/StatusBar.java
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
-
 import com.getcapacitor.Logger;
 
 public class StatusBar {
