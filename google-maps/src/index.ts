3d2faa1def86ec21eb99857ea12841dcac7e7906
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-04-25 09:10:19 -0400

    chore: fix eslint

--- google-maps/src/index.ts
+++ google-maps/src/index.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import { MapType } from './definitions';
 import { GoogleMap, Marker } from './map';
 
 export { GoogleMap, MapType, Marker };
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
 declare global {
   export namespace JSX {
     export interface IntrinsicElements {
