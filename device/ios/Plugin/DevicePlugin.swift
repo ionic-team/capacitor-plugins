10b45340f627c17aafdc8fdf11677b040d26959c
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-04-26 10:15:41 -0400

    chore: no force unwrap

--- device/ios/Plugin/DevicePlugin.swift
+++ device/ios/Plugin/DevicePlugin.swift
@@ -24,7 +24,7 @@ public class DevicePlugin: CAPPlugin {
         let diskFree = implementation.getFreeDiskSize() ?? 0
         let realDiskFree = implementation.getRealFreeDiskSize() ?? 0
         let diskTotal = implementation.getTotalDiskSize() ?? 0
-        let modelName = isSimulator ? ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"]! + " Simulator" : implementation.getModelName()
+        let modelName = isSimulator ? ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] : implementation.getModelName()
 
         call.resolve([
             "memUsed": memUsed,
