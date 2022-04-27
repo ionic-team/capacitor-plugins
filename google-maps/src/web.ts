34ea0e7b46af48a566d4a821fa841c850b02d018
Chace Daniels <chace@chaces-mbp.lan>
2022-04-27 10:53:59 -0700

    core web implementations

--- google-maps/src/web.ts
+++ google-maps/src/web.ts
@@ -25,7 +25,11 @@ export class CapacitorGoogleMapsWeb
 {
   private gMapsRef: typeof google.maps | undefined = undefined;
   private maps: {
-    [id: string]: { element: HTMLElement; map: google.maps.Map };
+    [id: string]: {
+      element: HTMLElement;
+      map: google.maps.Map;
+      trafficLayer?: google.maps.TrafficLayer;
+    };
   } = {};
 
   private async importGoogleLib(apiKey: string) {
@@ -42,55 +46,102 @@ export class CapacitorGoogleMapsWeb
     }
   }
 
-  setCamera(_args: CameraArgs): Promise<void> {
+  async setCamera(_args: CameraArgs): Promise<void> {
+    // Animation not supported yet...
+    this.maps[_args.id].map.moveCamera({
+      center: _args.config.coordinate,
+      heading: _args.config.bearing,
+      tilt: _args.config.angle,
+      zoom: _args.config.zoom,
+    });
+  }
+  
+  async setMapType(_args: MapTypeArgs): Promise<void> {
+    this.maps[_args.id].map.setMapTypeId(_args.mapType);
+  }
+  
+  async enableIndoorMaps(_args: IndoorMapArgs): Promise<void> {
+    throw new Error('Method not supported on web.');
+  }
+  
+  async enableTrafficLayer(_args: TrafficLayerArgs): Promise<void> {
+    const trafficLayer = this.maps[_args.id].trafficLayer ?? new google.maps.TrafficLayer();
+    
+    if (_args.enabled) {
+      trafficLayer.setMap(this.maps[_args.id].map);
+      this.maps[_args.id].trafficLayer = trafficLayer;
+    }
+    else if(this.maps[_args.id].trafficLayer) {
+      trafficLayer.setMap(null);
+      this.maps[_args.id].trafficLayer = undefined;
+    }
+  }
+  
+  async enableAccessibilityElements(_args: AccElementsArgs): Promise<void> {
+    throw new Error('Method not supported on web.');
+  }
+  
+  async enableCurrentLocation(_args: CurrentLocArgs): Promise<void> {
+    if (_args.enabled) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+
+            this.maps[_args.id].map.setCenter(pos);
+          },
+          () => {
+            throw new Error('Geolocation not supported on web browser.');
+          }
+        );
+      } else {
+        throw new Error('Geolocation not supported on web browser.');
+      }
+    }
+  }
+  async setPadding(_args: PaddingArgs): Promise<void> {
+    const bounds = this.maps[_args.id].map.getBounds();
+    
+    if (bounds !== undefined) {
+      this.maps[_args.id].map.fitBounds(bounds, _args.padding); 
+    }
+  }
+  async addMarkers(_args: AddMarkersArgs): Promise<{ ids: string[] }> {
     throw new Error('Method not implemented.');
   }
-  setMapType(_args: MapTypeArgs): Promise<void> {
+  
+  async removeMarkers(_args: RemoveMarkersArgs): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  enableIndoorMaps(_args: IndoorMapArgs): Promise<void> {
+  async enableClustering(_args: { id: string }): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  enableTrafficLayer(_args: TrafficLayerArgs): Promise<void> {
+  
+  async disableClustering(_args: { id: string }): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  enableAccessibilityElements(_args: AccElementsArgs): Promise<void> {
+  
+  async addMarker(_args: AddMarkerArgs): Promise<{ id: string }> {
     throw new Error('Method not implemented.');
   }
-  enableCurrentLocation(_args: CurrentLocArgs): Promise<void> {
+  
+  async removeMarker(_args: RemoveMarkerArgs): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  setPadding(_args: PaddingArgs): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  addMarkers(_args: AddMarkersArgs): Promise<{ ids: string[] }> {
-    throw new Error('Method not implemented.');
-  }
-  removeMarkers(_args: RemoveMarkersArgs): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  enableClustering(_args: { id: string }): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  disableClustering(_args: { id: string }): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  addMarker(_args: AddMarkerArgs): Promise<{ id: string }> {
-    throw new Error('Method not implemented.');
-  }
-  removeMarker(_args: RemoveMarkerArgs): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  onScroll(_args: OnScrollArgs): Promise<void> {
-    throw new Error('Method not implemented.');
+  
+  async onScroll(_args: OnScrollArgs): Promise<void> {
+    throw new Error('Method not supported on web.');
   }
 
-  async create(options: CreateMapArgs, _callback?: MapListenerCallback): Promise<void> {
-    console.log(`Create map: ${options.id}`);
-    await this.importGoogleLib(options.apiKey);
-    this.maps[options.id] = {
-      map: new window.google.maps.Map(options.element, { ...options.config }),
-      element: options.element,
+  async create(_args: CreateMapArgs): Promise<void> {
+    console.log(`Create map: ${_args.id}`);
+    await this.importGoogleLib(_args.apiKey);
+    this.maps[_args.id] = {
+      map: new window.google.maps.Map(_args.element, { ..._args.config }),
+      element: _args.element,
     };
   }
 
