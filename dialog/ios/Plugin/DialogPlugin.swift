5456cfdfd868e63dc389b59a40177690a04dae0c
jcesarmobile <jcesarmobile@gmail.com>
2022-04-27 16:31:33 +0200

    mandatory message

--- dialog/ios/Plugin/DialogPlugin.swift
+++ dialog/ios/Plugin/DialogPlugin.swift
@@ -9,7 +9,10 @@ public class DialogPlugin: CAPPlugin {
 
     @objc public func alert(_ call: CAPPluginCall) {
         let title = call.options["title"] as? String
-        let message = call.options["message"] as? String
+        guard let message = call.options["message"] as? String else {
+            call.reject("Please provide a message for the dialog")
+            return
+        }
         let buttonTitle = call.options["buttonTitle"] as? String ?? "OK"
 
         DispatchQueue.main.async { [weak self] in
@@ -23,7 +26,10 @@ public class DialogPlugin: CAPPlugin {
 
     @objc public func confirm(_ call: CAPPluginCall) {
         let title = call.options["title"] as? String
-        let message = call.options["message"] as? String ?? ""
+        guard let message = call.options["message"] as? String else {
+            call.reject("Please provide a message for the dialog")
+            return
+        }
         let okButtonTitle = call.options["okButtonTitle"] as? String ?? "OK"
         let cancelButtonTitle = call.options["cancelButtonTitle"] as? String ?? "Cancel"
 
@@ -45,7 +51,10 @@ public class DialogPlugin: CAPPlugin {
 
     @objc public func prompt (_ call: CAPPluginCall) {
         let title = call.options["title"] as? String
-        let message = call.options["message"] as? String ?? ""
+        guard let message = call.options["message"] as? String else {
+            call.reject("Please provide a message for the dialog")
+            return
+        }
         let okButtonTitle = call.options["okButtonTitle"] as? String ?? "OK"
         let cancelButtonTitle = call.options["cancelButtonTitle"] as? String ?? "Cancel"
         let inputPlaceholder = call.options["inputPlaceholder"] as? String ?? ""
