fdab9e38cfd43b7efcc39c5380051eb020b50dd1
jcesarmobile <jcesarmobile@gmail.com>
2023-01-30 19:07:38 +0100

    Update screen-orientation/ios/Plugin/ScreenOrientationPlugin.m

--- screen-orientation/ios/Plugin/ScreenOrientationPlugin.m
+++ screen-orientation/ios/Plugin/ScreenOrientationPlugin.m
@@ -5,4 +5,5 @@ CAP_PLUGIN(ScreenOrientationPlugin, "ScreenOrientation",
   CAP_PLUGIN_METHOD(orientation, CAPPluginReturnPromise);
   CAP_PLUGIN_METHOD(lock, CAPPluginReturnPromise);
   CAP_PLUGIN_METHOD(unlock, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnPromise);
 )
