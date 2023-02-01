0df60438798c4c2816455ed7e75563b25f24a944
jcesarmobile <jcesarmobile@gmail.com>
2023-02-01 20:51:21 +0100

    add web unavailable rejections

--- screen-orientation/src/web.ts
+++ screen-orientation/src/web.ts
@@ -12,21 +12,61 @@ export class ScreenOrientationWeb
 {
   constructor() {
     super();
-    window.screen.orientation.addEventListener('change', () => {
-      const type = window.screen.orientation.type;
-      this.notifyListeners('screenOrientationChange', { type });
-    });
+    if (
+      typeof screen !== 'undefined' &&
+      typeof screen.orientation !== 'undefined'
+    ) {
+      screen.orientation.addEventListener('change', () => {
+        const type = screen.orientation.type;
+        this.notifyListeners('screenOrientationChange', { type });
+      });
+    }
   }
 
   async orientation(): Promise<ScreenOrientationResult> {
-    return { type: window.screen.orientation.type };
+    if (typeof screen === 'undefined' || !screen.orientation) {
+      throw this.unavailable(
+        'ScreenOrientation API not available in this browser',
+      );
+    }
+    return { type: screen.orientation.type };
   }
 
   async lock(options: OrientationLockOptions): Promise<void> {
-    await window.screen.orientation.lock(options.orientation);
+    if (
+      typeof screen === 'undefined' ||
+      !screen.orientation ||
+      !screen.orientation.lock
+    ) {
+      throw this.unavailable(
+        'ScreenOrientation API not available in this browser',
+      );
+    }
+    try {
+      await screen.orientation.lock(options.orientation);
+    } catch {
+      throw this.unavailable(
+        'ScreenOrientation API not available in this browser',
+      );
+    }
   }
 
   async unlock(): Promise<void> {
-    window.screen.orientation.unlock();
+    if (
+      typeof screen === 'undefined' ||
+      !screen.orientation ||
+      !screen.orientation.unlock
+    ) {
+      throw this.unavailable(
+        'ScreenOrientation API not available in this browser',
+      );
+    }
+    try {
+      screen.orientation.unlock();
+    } catch {
+      throw this.unavailable(
+        'ScreenOrientation API not available in this browser',
+      );
+    }
   }
 }
