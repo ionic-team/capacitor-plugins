3de4718f2f437b982e04908f07b2b328d8247b6a
Troels <flakesdk@gmail.com>
2023-02-10 09:38:51 +0100

    Add no-op Keyboard web plugin

--- keyboard/src/index.ts
+++ keyboard/src/index.ts
@@ -2,7 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { KeyboardPlugin } from './definitions';
 
-const Keyboard = registerPlugin<KeyboardPlugin>('Keyboard');
+const Keyboard = registerPlugin<KeyboardPlugin>('Keyboard', {
+    web: () => import('./web').then(m => new m.KeyboardWeb()),
+  });
 
 export * from './definitions';
 export { Keyboard };
