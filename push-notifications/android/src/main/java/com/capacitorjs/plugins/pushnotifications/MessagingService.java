20f0ec19664daa806bca5b590ae6b4f5b3e584cc
agomezmart <60354460+agomezmart@users.noreply.github.com>
2023-03-16 10:35:27 +0100

    Include MKCloud SDK logic to MessageService

--- push-notifications/android/src/main/java/com/capacitorjs/plugins/pushnotifications/MessagingService.java
+++ push-notifications/android/src/main/java/com/capacitorjs/plugins/pushnotifications/MessagingService.java
@@ -3,13 +3,25 @@ package com.capacitorjs.plugins.pushnotifications;
 import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.salesforce.marketingcloud.MarketingCloudSdk;
+import com.salesforce.marketingcloud.messages.push.PushMessageManager;
 
 public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        PushNotificationsPlugin.sendRemoteMessage(remoteMessage);
+        if (PushMessageManager.isMarketingCloudPush(remoteMessage)) {
+            MarketingCloudSdk.requestSdk(new MarketingCloudSdk.WhenReadyListener() {
+                @Override
+                public void ready(@NonNull MarketingCloudSdk sdk) {
+                    sdk.getPushMessageManager().handleMessage(remoteMessage);
+                    PushNotificationsPlugin.sendRemoteMessage(remoteMessage);
+                }
+            });
+        } else {
+            PushNotificationsPlugin.sendRemoteMessage(remoteMessage);
+        }
     }
 
     @Override
