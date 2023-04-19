8e2d59605b71db01bdad89756381d1f14e7cde51
jcesarmobile <jcesarmobile@gmail.com>
2023-04-19 16:39:14 +0200

    chore: format with swiftlint 0.51.0

--- toast/ios/Plugin/ToastLabel.swift
+++ toast/ios/Plugin/ToastLabel.swift
@@ -3,7 +3,7 @@ import UIKit
 @objc internal class ToastLabel: UILabel {
     var padding: UIEdgeInsets?
 
-    override open func draw(_ rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         if let insets = padding {
             self.drawText(in: rect.inset(by: insets))
         } else {
