3c7f037eef613ba3cad9120210c7fbfe6f3cea43
drefrajo <71271762+drefrajo@users.noreply.github.com>
2023-06-18 10:27:55 +0200

    allow accessing security scoped resources

--- filesystem/ios/Plugin/Filesystem.swift
+++ filesystem/ios/Plugin/Filesystem.swift
@@ -22,11 +22,14 @@ import Foundation
     }
 
     public func readFile(at fileUrl: URL, with encoding: String?) throws -> String {
+        fileUrl.startAccessingSecurityScopedResource()
         if encoding != nil {
             let data = try String(contentsOf: fileUrl, encoding: .utf8)
+            fileUrl.stopAccessingSecurityScopedResource()
             return data
         } else {
             let data = try Data(contentsOf: fileUrl)
+            fileUrl.stopAccessingSecurityScopedResource()
             return data.base64EncodedString()
         }
     }
