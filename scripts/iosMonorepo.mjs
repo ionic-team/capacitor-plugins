5a3659f1530894579018555277bf029a0d58a8e2
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2023-07-14 11:55:44 -0400

    chore: fmt

--- scripts/iosMonorepo.mjs
+++ scripts/iosMonorepo.mjs
@@ -8,27 +8,26 @@ const searchPattern = '**/ios/Podfile';
 const searchString = '../node_modules/@capacitor/ios';
 const replacementString = '../../node_modules/@capacitor/ios';
 
-
 (async () => {
   try {
     // Search for all files that match the pattern
     const files = await glob(searchPattern);
     // For each file that matches the pattern
     for (const file of files) {
-        try {
-            // Read the file
-            let data = await fs.readFile(file, 'utf8');
+      try {
+        // Read the file
+        let data = await fs.readFile(file, 'utf8');
 
-            // Replace all instances of the search string with the replacement string
-            let result = data.split(searchString).join(replacementString);
+        // Replace all instances of the search string with the replacement string
+        let result = data.split(searchString).join(replacementString);
 
-            // Write the result back to the file
-            await fs.writeFile(file, result, 'utf8');
-        } catch (err) {
-            console.error("Error while processing file:", err);
-        }
+        // Write the result back to the file
+        await fs.writeFile(file, result, 'utf8');
+      } catch (err) {
+        console.error('Error while processing file:', err);
+      }
     }
   } catch (e) {
-    console.error("Error while searching for files:", e);
+    console.error('Error while searching for files:', e);
   }
 })();
