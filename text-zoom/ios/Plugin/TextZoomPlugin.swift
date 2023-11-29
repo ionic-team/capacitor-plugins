f19e89c5421d8a7b134467a35df616409390fe73
Mark Anderson <mark@ionic.io>
2023-11-29 14:59:21 -0500

    feat: Text-zoom SPM update

--- text-zoom/ios/Plugin/TextZoomPlugin.swift
+++ text-zoom/ios/Plugin/TextZoomPlugin.swift
@@ -2,7 +2,12 @@ import Foundation
 import Capacitor
 
 @objc(TextZoomPlugin)
-public class TextZoomPlugin: CAPPlugin {
+public class TextZoomPlugin: CAPPlugin, CAPBridgedPlugin {
+    public let identifier = "TextZoomPlugin" 
+    public let jsName = "TextZoom" 
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "getPreferred", returnType: CAPPluginReturnPromise),
+    ] 
     private let textZoom = TextZoom()
 
     @objc func getPreferred(_ call: CAPPluginCall) {
