3a7d7e18546508ce97a036371a51b59eb978338d
Joseph Pender <joey@ionic.io>
2023-12-08 15:57:22 -0600

    chore: bumping minimum iOS version to 14

--- status-bar/Package.swift
+++ status-bar/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorStatusBarPlugin",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "StatusBarPlugin",
