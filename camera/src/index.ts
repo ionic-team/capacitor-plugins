9fc5fa44e60cd3dc902287f5175d182bba4ebb4e
Stephan Fischer <stephan@mrfischer.de>
2023-12-30 22:12:12 +0100

    bug(camera): pickImages() doesn't work first time on safari #1314

--- camera/src/index.ts
+++ camera/src/index.ts
@@ -1,9 +1,11 @@
 import { registerPlugin } from '@capacitor/core';
 
 import type { CameraPlugin } from './definitions';
+// See https://github.com/ionic-team/capacitor-plugins/issues/1314
+import * as web from './web';
 
 const Camera = registerPlugin<CameraPlugin>('Camera', {
-  web: () => import('./web').then(m => new m.CameraWeb()),
+  web: () => new web.CameraWeb(),
 });
 
 export * from './definitions';
