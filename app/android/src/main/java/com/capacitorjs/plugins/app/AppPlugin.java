f69604ee14f7583a42aeb8202f0fcb2f94cb2f2e
Sebastian Kempken <sebastian@kempken.io>
2024-01-17 19:46:00 +0100

    refactor(app)!: use moveTaskToBack within minimizeApp (#1956)
    Co-authored-by: jcesarmobile <jcesarmobile@gmail.com>

--- app/android/src/main/java/com/capacitorjs/plugins/app/AppPlugin.java
+++ app/android/src/main/java/com/capacitorjs/plugins/app/AppPlugin.java
@@ -108,10 +108,7 @@ public class AppPlugin extends Plugin {
 
     @PluginMethod
     public void minimizeApp(PluginCall call) {
-        Intent startMain = new Intent(Intent.ACTION_MAIN);
-        startMain.addCategory(Intent.CATEGORY_HOME);
-        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        getActivity().startActivity(startMain);
+        getActivity().moveTaskToBack(true);
         call.resolve();
     }
 
