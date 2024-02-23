3aee56805ba5638388bb7f080ff3df7316ee6a43
Mark Anderson <mark@ionic.io>
2024-02-23 12:51:25 -0500

    action-sheet update

--- action-sheet/Package.swift
+++ action-sheet/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "ActionSheetPlugin",
+            name: "CapacitorActionSheet",
             targets: ["ActionSheetPlugin"])
     ],
     dependencies: [
