8ccf2c5205e0b3d9bd4c435d4fc448a4f1f5d4d8
Mark Anderson <mark@ionic.io>
2024-02-27 14:14:04 -0500

    chore: fix toast compilation

--- toast/Package.swift
+++ toast/Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     products: [
         .library(
             name: "CapacitorToast",
-            targets: ["CapacitorToast"])
+            targets: ["ToastPlugin"])
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-spm.git", branch: "main")
     ],
     targets: [
         .target(
-            name: "CapacitorToast",
+            name: "ToastPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-spm"),
                 .product(name: "Cordova", package: "capacitor-spm")
