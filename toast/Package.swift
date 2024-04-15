b65ba06a9318b9d531f81698e683c0ce1763c1c4
Steven Sherry <steven.sherry@ionic.io>
2024-04-15 09:06:16 -0500

    chore: Update spm repo (#2079)

--- toast/Package.swift
+++ toast/Package.swift
@@ -10,14 +10,14 @@ let package = Package(
             targets: ["ToastPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-spm.git", branch: "main")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
     ],
     targets: [
         .target(
             name: "ToastPlugin",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-spm"),
-                .product(name: "Cordova", package: "capacitor-spm")
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
             path: "ios/Sources/ToastPlugin"),
         .testTarget(
