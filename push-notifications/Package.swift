1a4e989320f3ae8faa89c9e5de4b2df4cff01081
OS-ruimoreiramendes <rui.moreira.mendes@outsystems.com>
2025-11-14 17:02:03 +0000

    chore(ios): update spm from 7.0.0 to 8.0.0-beta (#2434)

--- push-notifications/Package.swift
+++ push-notifications/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["PushNotificationsPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0-beta")
     ],
     targets: [
         .target(
