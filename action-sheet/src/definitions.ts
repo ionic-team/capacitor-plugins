e9531a98a3d68dbc202fd61fbfbb448ded935df3
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-19 18:36:22 +0000

    docs: Update new parameters documentation

--- action-sheet/src/definitions.ts
+++ action-sheet/src/definitions.ts
@@ -26,8 +26,10 @@ export interface ShowActionsOptions {
    * If true, sheet is canceled when clicked outside; If false, it is not. By default, false.
    *
    * On iOS, the sheet is also cancelable if a button with ActionSheetButtonStyle.Cancel is provided and cancelable is false.
+   * 
+   * On Web, requires having @ionic/pwa-elements version 3.4.0 or higher.
    *
-   * @since 6.1.0
+   * @since 8.1.0
    */
   cancelable?: boolean;
 }
@@ -94,8 +96,10 @@ export interface ShowActionsResult {
   index: number;
   /**
    * True if sheet was canceled by user; False otherwise
-   *
-   * @since 6.1.0
+   * 
+   * On Web, requires having @ionic/pwa-elements version 3.4.0 or higher.
+   * 
+   * @since 8.1.0
    */
   canceled: boolean;
 }
