c713b98d0598e7e53b6347be05a341225f31a44d
jcesarmobile <jcesarmobile@gmail.com>
2026-06-16 15:09:58 +0200

    chore(status-bar): format Package.swift

--- status-bar/Package.swift
+++ status-bar/Package.swift
@@ -18,7 +18,8 @@ let package = Package(
             name: "StatusBarPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")],
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
             path: "ios/Sources/StatusBarPlugin"
         ),
         .testTarget(
