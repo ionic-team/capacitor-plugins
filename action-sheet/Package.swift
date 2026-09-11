2f506f84007b3ccf024050b9d0466e639437999d
jcesarmobile <jcesarmobile@gmail.com>
2026-09-11 16:47:04 +0200

    chore: point SPM to Capacitor source reposotory

--- action-sheet/Package.swift
+++ action-sheet/Package.swift
@@ -10,13 +10,13 @@ let package = Package(
             targets: ["ActionSheetPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5")
+        .package(url: "https://github.com/ionic-team/capacitor.git", from: "9.0.0-alpha.7")
     ],
     targets: [
         .target(
             name: "ActionSheetPlugin",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm")
+                .product(name: "Capacitor", package: "capacitor")
             ],
             path: "ios/Sources/ActionSheetPlugin"),
         .testTarget(
