f5aaa329091286d0ce25642fff4dc6e5b4fbaf65
jcesarmobile <jcesarmobile@gmail.com>
2026-09-22 11:01:19 +0200

    chore: point SPM to Capacitor source reposotory (#2600)

--- text-zoom/Package.swift
+++ text-zoom/Package.swift
@@ -10,13 +10,13 @@ let package = Package(
             targets: ["TextZoomPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5")
+        .package(url: "https://github.com/ionic-team/capacitor.git", from: "9.0.0-alpha.7")
     ],
     targets: [
         .target(
             name: "TextZoomPlugin",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm")
+                .product(name: "Capacitor", package: "capacitor")
             ],
             path: "ios/Sources/TextZoomPlugin"),
         .testTarget(
